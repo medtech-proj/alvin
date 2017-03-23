ab3a3a61b25ccd8175937ce3227d4340d89e31b1
lisa <s2luminous@gmail.com>
2017-03-23 14:59:23 -0400

    fix sql statements

--- dbseed.py
+++ dbseed.py
@@ -13,72 +13,65 @@ database = 'test'
 connection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
 
 #create cursor factory
-connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+# connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 
-cursor = connection.cursor(cursor_factory=RealDictCursor)
+cursor = connection.cursor()#cursor_factory=RealDictCursor)
 
-#drop tables if exists...?
 # cursor.execute('CREATE DATABASE test;')
 
 cursor.execute('''
-	DROP TABLE IF EXISTS 'procedures';
+	DROP TABLE IF EXISTS procedure_type;
+''')
+
+cursor.execute('''
+	CREATE TABLE procedure_type (
+	id SERIAL PRIMARY KEY,
+	cpt_code VARCHAR,
+	description TEXT
+	);
+''')
+
+cursor.execute('''
+	DROP TABLE IF EXISTS facilities;
+''')
+
+cursor.execute('''
+	CREATE TABLE facilities (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	address TEXT,
+	image BYTEA,
+	reviews TEXT
+	);
+''')
+
+cursor.execute('''
+	DROP TABLE IF EXISTS geolcations;
+''')
+
+cursor.execute('''
+	CREATE TABLE geolocations (
+	id SERIAL PRIMARY KEY,
+	id_facilities INTEGER,
+	latitude VARCHAR,
+	longitude VARCHAR,
+	FOREIGN KEY (id_facilities) REFERENCES facilities(id)
+	);
+''')
+
+cursor.execute('''
+	DROP TABLE IF EXISTS procedures;
 	''')
 
 cursor.execute('''
-	CREATE TABLE 'procedures' (
-	'id' INTEGER PRIMARY KEY AUTO_INCREMENT DEFAULT NOT NULL,
-	'id_procedure_types' INTEGER NULL DEFAULT NULL,
-	'id_facilities' INTEGER NULL DEFAULT NULL,
-	'tot_price' INTEGER NULL DEFAULT NULL,
-	FOREIGN KEY (id_procedure_types) REFERENCES 'procedure_type' ('id'),
-	FOREIGN KEY (id_facilities) REFERENCES 'facilities' ('id')
-
+	CREATE TABLE procedures (
+	id SERIAL PRIMARY KEY,
+	id_procedure_types INTEGER,
+	id_facilities INTEGER,
+	tot_price INTEGER,
+	FOREIGN KEY (id_procedure_types) REFERENCES procedure_type(id),
+	FOREIGN KEY (id_facilities) REFERENCES facilities(id)
 	);
-
-''')
-
-cursor.execute('''
-	DROP TABLE IF EXISTS 'procedure_type';
-''')
-
-cursor.execute('''
-	CREATE TABLE 'procedure_type' (
-	'id' INTEGER PRIMARY KEY AUTO_INCREMENT DEFAULT NOT NULL,
-	'cpt_code' VARCHAR NULL DEFAULT NULL,
-	'description' MEDIUMTEXT NULL DEFAULT NULL
-	);
-
-''')
-
-cursor.execute('''
-	DROP TABLE IF EXISTS 'facilities';
-''')
-
-cursor.execute('''
-	CREATE TABLE 'facilities' (
-	'id' INTEGER PRIMARY KEY AUTO_INCREMENT DEFAULT NOT NULL,
-	'name' MEDIUMTEXT NULL DEFAULT NULL,
-	'address' MEDIUMTEXT NULL DEFAULT NULL,
-	'image' BLOB NULL DEFAULT NULL,
-	'reviews' MEDIUMTEXT NULL DEFAULT NULL
-	);
-''')
-
-cursor.execute('''
-	DROP TABLE IF EXISTS 'geolcations';
-''')
-
-cursor.execute('''
-	CREATE TABLE 'geolocations' (
-	'id' INTEGER PRIMARY KEY AUTO_INCREMENT DEFAULT NOT NULL,
-	'id_facilities' INTEGER NULL DEFAULT NULL,
-	'latitude' VARCHAR NULL DEFAULT NULL,
-	'longitude' VARCHAR NULL DEFAULT NULL,
-	FOREIGN KEY (id_facilities) REFERENCES 'facilities' ('id')
-	);
-''')
-
-cursor.execute('''
 ''')
 
 
