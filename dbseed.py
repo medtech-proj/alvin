a4c2b04e030fac6f3d921d18d89b41b488de1211
Vincent Chiodo <vincent@DA09-Vincent-MacBookAir.local>
2017-03-23 14:39:05 -0400

    update db file

--- dbseed.py
+++ dbseed.py
@@ -20,12 +20,68 @@ cursor = connection.cursor(cursor_factory=RealDictCursor)
 #drop tables if exists...?
 # cursor.execute('CREATE DATABASE test;')
 
+cursor.execute('''
+	DROP TABLE IF EXISTS 'procedures';
+	''')
 
 cursor.execute('''
-	CREATE TABLE test_tables (
-	id serial PRIMARY KEY
-	);'''
-	)
+	CREATE TABLE 'procedures' (
+	'id' INTEGER PRIMARY KEY AUTO_INCREMENT DEFAULT NOT NULL,
+	'id_procedure_types' INTEGER NULL DEFAULT NULL,
+	'id_facilities' INTEGER NULL DEFAULT NULL,
+	'tot_price' INTEGER NULL DEFAULT NULL,
+	FOREIGN KEY (id_procedure_types) REFERENCES 'procedure_type' ('id'),
+	FOREIGN KEY (id_facilities) REFERENCES 'facilities' ('id')
+
+	);
+
+''')
+
+cursor.execute('''
+	DROP TABLE IF EXISTS 'procedure_type';
+''')
+
+cursor.execute('''
+	CREATE TABLE 'procedure_type' (
+	'id' INTEGER PRIMARY KEY AUTO_INCREMENT DEFAULT NOT NULL,
+	'cpt_code' VARCHAR NULL DEFAULT NULL,
+	'description' MEDIUMTEXT NULL DEFAULT NULL
+	);
+
+''')
+
+cursor.execute('''
+	DROP TABLE IF EXISTS 'facilities';
+''')
+
+cursor.execute('''
+	CREATE TABLE 'facilities' (
+	'id' INTEGER PRIMARY KEY AUTO_INCREMENT DEFAULT NOT NULL,
+	'name' MEDIUMTEXT NULL DEFAULT NULL,
+	'address' MEDIUMTEXT NULL DEFAULT NULL,
+	'image' BLOB NULL DEFAULT NULL,
+	'reviews' MEDIUMTEXT NULL DEFAULT NULL
+	);
+''')
+
+cursor.execute('''
+	DROP TABLE IF EXISTS 'geolcations';
+''')
+
+cursor.execute('''
+	CREATE TABLE 'geolocations' (
+	'id' INTEGER PRIMARY KEY AUTO_INCREMENT DEFAULT NOT NULL,
+	'id_facilities' INTEGER NULL DEFAULT NULL,
+	'latitude' VARCHAR NULL DEFAULT NULL,
+	'longitude' VARCHAR NULL DEFAULT NULL,
+	FOREIGN KEY (id_facilities) REFERENCES 'facilities' ('id')
+	);
+''')
+
+cursor.execute('''
+''')
+
+
 
 connection.commit()
 connection.close()
