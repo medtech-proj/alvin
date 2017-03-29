92e57731d32022d8206f1dcdfd400a3797fe139b
lgibson212 <lgibson212@users.noreply.github.com>
2017-03-29 13:56:34 -0400

    search file works

--- search.py
+++ search.py
@@ -1,6 +1,39 @@
-joindb = select * from (SELECT * 
-FROM procedures 
-JOIN facilities 
-ON procedures.facility_name = facilities.name
-JOIN procedure_type
-ON procedure_type.cpt_code = facilities.cpt_code) 
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+
+hostname = 'localhost'
+username = 'Lisa' #postgres is the owner in psql 
+password = 'secret'
+database = 'test'
+
+#creates db
+connection = psycopg2.connect(dbname=database)
+
+#create cursor factory
+connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+cursor = connection.cursor(cursor_factory=RealDictCursor)
+
+
+
+search =  "CT Head W W/O Contrast"
+
+cursor.execute('''
+	SELECT name, address, description, tot_price, image, rating, reviews
+	FROM procedures 
+
+	JOIN procedure_types
+	ON procedure_types.id = procedures.id_procedure_types
+
+	JOIN facilities 
+	ON procedures.id_facilities = facilities.id
+
+	WHERE procedure_types.description like 'CT%';
+''')
+query = cursor.fetchall()
+print(query)
+# return query
+
+
+connection.close()
