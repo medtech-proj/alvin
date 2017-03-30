9968f108fb745faf40472a2775e1720ca3fb17a4
Vincent Chiodo <vincent@DA09-Vincent-MacBookAir.local>
2017-03-30 18:01:31 -0400

    routes

--- controller.py
+++ controller.py
@@ -1,5 +1,8 @@
 from flask import Flask, redirect, render_template, session, request, url_for
-# import model
+import search
+# from search import get_procedure_info
+# import alvin_geos
+import json
 
 app = Flask(__name__)
 
@@ -13,12 +16,30 @@ def show_healthmap():
 	return render_template("map.html")
 
 @app.route("/procedure/<name>")
-def get_hos_name():
-	data=query(name)
+def get_hosp_name(name):
+	data = search.get_procedure_info(name)
 	# return httpResponse(json.dumps(data))
-	return json.dumps(data)
+	# for _ in data:
+	for obj in data:
+		x = obj['name']
+		address = obj['address']
+		description = obj['description']
+		tot_price = obj['tot_price']
+		image = obj['image']
+		rating = obj['rating']
+		reviews = obj['reviews']
+
+
+		print(x)
+
+
+	# print(data[0]['name'])
+	# print(type(data))
+	# result = {'result':str(data)}
+
+	# return json.dumps(result)
 
 
 
 if __name__ == "__main__":
-	app.run(host="127.0.0.1", port=5000)
+	app.run(host="127.0.0.1", debug=True, port=5000)
--- search.py
+++ search.py
@@ -7,33 +7,44 @@ username = 'Lisa' #postgres is the owner in psql
 password = 'secret'
 database = 'test'
 
+
+def get_procedure_info(procedure):
 #creates db
-connection = psycopg2.connect(dbname=database)
+	connection = psycopg2.connect(dbname=database)
 
 #create cursor factory
-connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+	connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 
-cursor = connection.cursor(cursor_factory=RealDictCursor)
-
-
-
-search =  "CT Head W W/O Contrast"
-
-cursor.execute('''
-	SELECT name, address, description, tot_price, image, rating, reviews
-	FROM procedures 
-
-	JOIN procedure_types
-	ON procedure_types.id = procedures.id_procedure_types
-
-	JOIN facilities 
-	ON procedures.id_facilities = facilities.id
-
-	WHERE procedure_types.description like 'CT%';
-''')
-query = cursor.fetchall()
-print(query)
-# return query
+	cursor = connection.cursor(cursor_factory=RealDictCursor)
 
 
-connection.close()
+
+# search =  "CT Head W W/O Contrast"
+
+
+	# print(procedure)
+	data = str(procedure)+'%'
+	cursor.execute('''
+		SELECT name, address, description, tot_price, image, rating, reviews
+		FROM procedures 
+
+		JOIN procedure_types
+		ON procedure_types.id = procedures.id_procedure_types
+
+		JOIN facilities 
+		ON procedures.id_facilities = facilities.id
+
+		WHERE procedure_types.description LIKE (%s);
+	''', (data,))
+	# print(data)
+	query = cursor.fetchall()
+	print(query)
+	return query
+	# return query
+
+# get_procedure_info('CT')
+
+
+	connection.close()
+
+
